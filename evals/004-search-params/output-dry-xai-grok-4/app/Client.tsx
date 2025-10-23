5348bf1bacf1714c4e75d4fc123afe3f98ef0648
William Simmonds <wjsimmonds@gmail.com>
2025-10-23 16:28:31 -0300

    Remove output folders.

--- evals/004-search-params/output-dry-xai-grok-4/app/Client.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-
-export default function Client({ name }: { name: string }) {
-  return <div data-testid="client">Client component with name: {name}</div>;
-}
--- evals/006-server-metadata/output-dry-xai-grok-4/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// TODO: Export metadata object with title "My Page" and description "Test"
-
-export default function Page() {
-  return (
-    <div>
-      Page Content
-    </div>
-  );
-}
